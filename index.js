dcfb1e5fcc0555ae246873f6796803b13450c6e8
Divya Chaudhary <divyachaudhary879@gmail.com>
2025-06-07 19:09:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+ document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const message = document.getElementById('message').value.trim();
+
+  if (name && email && message) {
+    alert(`Thanks ${name}! Your message has been sent.`);
+    this.reset();
+  } else {
+    alert("Please fill in all fields.");
+  }
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const resumeBtn = document.getElementById("resumeBtn");
+    resumeBtn.addEventListener("click", function () {
+        alert("Your resume will start downloading!");
+    });
+});
+
+
